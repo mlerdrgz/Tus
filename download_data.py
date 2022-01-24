b9ca49a63dd5d04fee09df5a069d9504c6a40458
Emily Rodriguez <41804020+mlerdrgz@users.noreply.github.com>
2022-01-24 16:14:42 -0700

    Delete download_data.py

--- download_data.py
+++
@@ -1,9 +0,0 @@
-import obspy
-from obspy.clients.fdsn import Client
-from obspy import UTCDateTime
-client = Client("IRIS")
-t = UTCDateTime("2022-01-24 20:00:00") # UTC = +8 hrs from Tucson
-[ sbef, saft ] = [ 0, 3600 ] # seconds before and after start time (t)
-st = client.get_waveforms( "IU", "TUC", "00", "BH*", t - sbef, t + saft)
-st.filter("bandpass", freqmin=1.0, freqmax=5.0, corners=2, zerophase=False)
-st.plot( linewidth=0.75, equal_scale=False, starttime = t)
